90d9616863bf23624704f5c02297063a5a1bc8d1
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-01-08 03:21:48 +0000

    [CI] gettext not needed as a build dep

--- .github/installBuildDependencies.sh
+++ .github/installBuildDependencies.sh
@@ -4,7 +4,3 @@ set -eu
 # python3-setuptools and python3-wheel are required to pip install meson
 sudo apt-get --quiet install --yes python3-setuptools python3-wheel
 sudo -H pip3 install meson
-
-# gettext is required by libfontforge
-sudo apt-get --quiet install --yes gettext
-
