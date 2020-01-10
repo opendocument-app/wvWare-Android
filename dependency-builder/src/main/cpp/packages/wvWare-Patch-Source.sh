1ba769763337a5ef83d1b015deb81c9f79ae7739
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-01-10 03:25:16 +0000

    Remove wvWare.exe patch. we JNI now, no need for (barely) working .exe's

--- dependency-builder/src/main/cpp/packages/wvWare-Patch-Source.sh
+++ dependency-builder/src/main/cpp/packages/wvWare-Patch-Source.sh
@@ -6,9 +6,6 @@ BASEDIR=$(dirname "$THIS_FILE")
 
 patch $1/wvRTF.c $BASEDIR/wvWare-Patch-Source-wvRTF.c.patch
 
-patch -p0 <$BASEDIR/wvWare-Patch-Source-android.patch
-
 rm $1/config.sub
 
 NOCONFIGURE=1 $1/autogen.sh
-
