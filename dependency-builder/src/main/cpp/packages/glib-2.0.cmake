fc8e0da502dd4ad8caf5110dad02583b690b947f
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-01-10 18:58:04 +0000

    Update GLib to 2.62.4

--- dependency-builder/src/main/cpp/packages/glib-2.0.cmake
+++ dependency-builder/src/main/cpp/packages/glib-2.0.cmake
@@ -2,8 +2,8 @@ include_guard(GLOBAL)
 
 ExternalProjectMeson(glib-2.0
   DEPENDS iconv intl libffi zlib
-  URL https://download.gnome.org/sources/glib/2.62/glib-2.62.3.tar.xz
-  URL_HASH SHA256=4400adc9f0d3ffcfe8e84225210370ce3f9853afb81812ddadb685325aa655c4
+  URL https://download.gnome.org/sources/glib/2.62/glib-2.62.4.tar.xz
+  URL_HASH SHA256=4c84030d77fa9712135dfa8036ad663925655ae95b1d19399b6200e869925bbc
   CONFIGURE_ARGUMENTS -Dlibmount=false
 )
 
