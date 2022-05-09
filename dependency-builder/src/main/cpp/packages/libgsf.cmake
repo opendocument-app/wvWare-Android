547e7ebd72bdb71980da945e2aa1ad3f3c072f85
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-05-09 13:37:31 +0300

    [Deps] Bump libgsf to 1.14.49

--- dependency-builder/src/main/cpp/packages/libgsf.cmake
+++ dependency-builder/src/main/cpp/packages/libgsf.cmake
@@ -6,8 +6,8 @@ include_guard(GLOBAL)
 ExternalProjectAutotools(libgsf
   DEPENDS glib-2.0 libxml-2.0 zlib
 
-  URL https://ftp.gnome.org/pub/GNOME/sources/libgsf/1.14/libgsf-1.14.46.tar.xz
-  URL_HASH SHA256=ea36959b1421fc8e72caa222f30ec3234d0ed95990e2bf28943a85f33eadad2d
+  URL https://ftp.gnome.org/pub/GNOME/sources/libgsf/1.14/libgsf-1.14.49.tar.xz
+  URL_HASH SHA256=e9ebe36688f010c9e6e40c8903f3732948deb8aca032578d07d0751bd82cf857
 
   CONFIGURE_ARGUMENTS --disable-largefile
 )
