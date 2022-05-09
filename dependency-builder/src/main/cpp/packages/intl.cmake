3b0ec6deb23cab8808272605f2b4e2da4bb05e78
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-05-09 14:16:42 +0300

    [2/2][Deps] Bump proxy-libintl to 0.4.1

--- dependency-builder/src/main/cpp/packages/intl.cmake
+++ dependency-builder/src/main/cpp/packages/intl.cmake
@@ -1,7 +1,6 @@
 include_guard(GLOBAL)
 
 ExternalProjectMeson(intl
-  #@TODO: backport
   URL https://github.com/ViliusSutkus89/proxy-libintl/archive/refs/tags/0.4.1.tar.gz
   URL_HASH SHA256=89d72a5cdd6df29beb6cf1aa7284570ff8f4265185ecbf44827b741a864bbec9
 )
