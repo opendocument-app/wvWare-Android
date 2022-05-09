9319f9be20411c4c6630300571c23a061fd64076
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-05-09 12:11:04 +0300

    [Dependencies] Bump freetype to 2.12.1

--- dependency-builder/src/main/cpp/packages/freetype.cmake
+++ dependency-builder/src/main/cpp/packages/freetype.cmake
@@ -2,8 +2,8 @@ include_guard(GLOBAL)
 
 ExternalProjectAutotools(freetype
   DEPENDS libpng zlib
-  URL https://download.sourceforge.net/freetype/freetype-2.10.1.tar.xz
-  URL_HASH SHA256=16dbfa488a21fe827dc27eaf708f42f7aa3bb997d745d31a19781628c36ba26f
+  URL https://download.sourceforge.net/freetype/freetype-2.12.1.tar.xz
+  URL_HASH SHA256=4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f
 
   CONFIGURE_ARGUMENTS --with-zlib=yes --with-bzip2=no
     --with-png=yes
