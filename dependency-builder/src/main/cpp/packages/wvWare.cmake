7480429d25c904ec8b0abe74db32133fb9d43038
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-01-01 18:29:12 +0200

    [2/2] NDK26 has stricter default compiler errors. Disable incompatible-function-pointer-types error for wvWare

--- dependency-builder/src/main/cpp/packages/wvWare.cmake
+++ dependency-builder/src/main/cpp/packages/wvWare.cmake
@@ -6,6 +6,7 @@ ExternalProjectAutotools(wvWare
   URL https://www.abisource.com/downloads/wv/1.2.9/wv-1.2.9.tar.gz
   URL_HASH SHA256=4c730d3b325c0785450dd3a043eeb53e1518598c4f41f155558385dd2635c19d
 
-  CONFIGURE_ARGUMENTS --with-libwmf=${THIRDPARTY_PREFIX} -Wno-error=incompatible-function-pointer-types
+  CONFIGURE_ARGUMENTS --with-libwmf=${THIRDPARTY_PREFIX}
+  EXTRA_ENVVARS "CFLAGS=-Wno-error=incompatible-function-pointer-types"
 )
 
